b5021d202cd60874388c8e07ce7623371a70208f
pablogarces16 <pablogarcesee@gmail.com>
2017-10-29 19:52:24 -0400

    Added firebase cloud functions, moved files

---
+++ functions/index.js
@@ -0,0 +1,63 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+
+// When a new user is added under applicants
+exports.subscribeUser = functions.database.ref('applicants/{id}').onCreate(event => {
+
+  var user = event.data.val();
+  var id = event.params.id;
+  var fetch = require('isomorphic-fetch');
+  var btoa = require('btoa');
+
+  // Exit if the user was deleted or the user is currently subscribed
+  // if (!event.data.exists() || user.subscribed) {
+  //   return
+  // }
+
+  function createFetch() {
+
+    var MAILCHIMP_API_KEY = 'YW55c3RyaW5nOmRiOWZjYWZmOTJlNzU4YzdkYzcxMjE0MWZkNmMzODU5LXVzMTA=';
+
+    var url = 'https://us10.api.mailchimp.com/3.0/lists/fda6f8d99c/members';
+    var method = 'POST';
+    var headers = {
+      'authorization': "Basic " + btoa('randomstring:' + MAILCHIMP_API_KEY),
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+    var body = JSON.stringify(
+
+      {
+        // Adding just email
+        email_address: user.email,
+        status: 'subscribed'
+
+        // Adding email and name (want to see if above works first)
+        // email_address: user.email,
+        // status: 'subscribed',
+        // merge_fields: {
+				// 	FNAME: fname,
+				// 	LNAME: lname
+				// }
+      }
+
+    );
+
+    return fetch(url, {
+      method,
+      headers,
+      body
+    }).then(resp => resp.json()).then(resp => {
+      console.log(resp)
+      admin.database().ref('applicants/' + id + '/subscribed').set(true);
+    });
+  }
+
+  return createFetch();
+
+})
